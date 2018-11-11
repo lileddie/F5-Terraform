2a5c296884131e5a634df6515497fb88d575c774
Troy Schmid <troy.edward.schmid@gmail.com>
2018-11-11 15:50:11 -0800

    Added 2nd monitor

--- main.tf
+++ main.tf
@@ -3,11 +3,20 @@ provider "bigip" {
   username = "terraform"
 }
 
-resource "bigip_ltm_monitor" "monitor" {
-  name = "/Common/http_monitor"
+resource "bigip_ltm_monitor" "monitor80" {
+  name = "/Common/http_monitor_80"
   parent = "/Common/http"
   send = "GET /status.html\r\n"
   timeout = "46"
   interval = "15"
   destination = "*:80"
+}
+
+resource "bigip_ltm_monitor" "monitor8080" {
+  name = "/Common/http_monitor_8080"
+  parent = "/Common/http"
+  send = "GET /status.html\r\n"
+  timeout = "46"
+  interval = "15"
+  destination = "*:8080"
 }
