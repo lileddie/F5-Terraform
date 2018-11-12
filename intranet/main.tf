35b1b7d298f90aca54eec505db44c45e896346ae
Troy Schmid <troy.edward.schmid@gmail.com>
2018-11-11 21:05:15 -0800

    Updating pool syntax

--- intranet/main.tf
+++ intranet/main.tf
@@ -31,16 +31,10 @@ resource "bigip_ltm_node" "intra3" {
 resource "bigip_ltm_pool" "intraPool" {
   name = "/Common/intraPool"
   load_balancing_mode = "round-robin"
+  nodes: ["intra1:80","intra2:80","intra3:80"]
   monitors = ["/Common/http_monitor_80"]
   allow_snat = "yes"
   allow_nat = "yes"
-}
-
-resource "bigip_ltm_pool_attachment" "node-intraPool" {
-  pool = "/Common/intraPool"
-  node = "/Common/intra1:80"
-  node = "/Common/intra2:80"
-  node = "/Common/intra3:80"
 }
 
 resource "bigip_ltm_virtual_server" "https" {
