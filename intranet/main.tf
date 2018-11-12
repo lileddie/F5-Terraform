83146fb75990a4453048b730dd63d17d10f67791
Troy Schmid <troy.edward.schmid@gmail.com>
2018-11-11 19:22:32 -0800

    Config added

---
+++ intranet/main.tf
@@ -0,0 +1,56 @@
+#----- template for intranet VIPs -----#
+provider "bigip" {
+  address = "${var.f5_host}"
+  username = "${var.f5_user}"
+  password = "${var.f5_pass}"
+}
+
+resource "bigip_ltm_monitor" "intranet" {
+  name = "/Common/http_monitor_80"
+  parent = "/Common/http"
+  send = "GET /status.html\r\n"
+  timeout = "46"
+  interval = "15"
+  destination = "*:80"
+}
+
+#node creation
+resource "bigip_ltm_node" "intra1" {
+  name = "/Common/intra1"
+  address = "10.32.10.64"
+}
+resource "bigip_ltm_node" "intra2" {
+  name = "/Common/intra2"
+  address = "10.32.10.65"
+}
+resource "bigip_ltm_node" "intra3" {
+  name = "/Common/intra2"
+  address = "10.32.10.66"
+}
+
+resource "bigip_ltm_pool" "intraPool" {
+  name = "/Common/intraPool"
+  load_balancing_mode = "round-robin"
+  monitors = ["/Common/http_monitor_80"]
+  allow_snat = "yes"
+  allow_nat = "yes"
+}
+
+resource "bigip_ltm_pool_attachment" "node-intraPool" {
+  pool = "/Common/intraPool"
+  node = "${bigip_ltm_node.node.intra1}:80"
+  node = "${bigip_ltm_node.node.intra2}:80"
+  node = "${bigip_ltm_node.node.intra2}:80"
+}
+
+resource "bigip_ltm_virtual_server" "https" {
+  name = "/Common/intranet_https"
+  destination = "10.33.7.23"
+  port = 443
+  pool = "/Common/intraPool"
+  profiles = ["/Common/tcp","/Common/inranetSSL","/Common/http"]
+  source_address_translation = "automap"
+  translate_address = "enabled"
+  translate_port = "enabled"
+  vlans_disabled = true
+}
---
+++ www-static-content/main.tf
@@ -0,0 +1,68 @@
+#----- template for intranet VIPs -----#
+provider "bigip" {
+  address = "${var.f5_host}"
+  username = "${var.f5_user}"
+  password = "${var.f5_pass}"
+}
+
+resource "bigip_ltm_monitor" "www-static-content" {
+  name = "/Common/http_monitor_8080"
+  parent = "/Common/http"
+  send = "GET /serverstatus.html\r\n"
+  timeout = "46"
+  interval = "15"
+  destination = "*:8080"
+}
+
+#node creation
+resource "bigip_ltm_node" "intra1" {
+  name = "/Common/wwwStatic1"
+  address = "10.32.11.64"
+}
+resource "bigip_ltm_node" "intra2" {
+  name = "/Common/wwwStatic2"
+  address = "10.32.11.65"
+}
+resource "bigip_ltm_node" "intra3" {
+  name = "/Common/wwwStatic3"
+  address = "10.32.11.66"
+}
+
+resource "bigip_ltm_pool" "wwwStaticPool" {
+  name = "/Common/wwwStaticPool"
+  load_balancing_mode = "round-robin"
+  monitors = ["/Common/http_monitor_8080"]
+  allow_snat = "yes"
+  allow_nat = "yes"
+}
+
+resource "bigip_ltm_pool_attachment" "node-wwwStaticPool" {
+  pool = "/Common/wwwStaticPool"
+  node = "${bigip_ltm_node.node.wwwStatic1}:8080"
+  node = "${bigip_ltm_node.node.wwwStatic2}:8080"
+  node = "${bigip_ltm_node.node.wwwStatic3}:8080"
+}
+
+resource "bigip_ltm_virtual_server" "https" {
+  name = "/Common/wwwStatic_https"
+  destination = "10.33.8.23"
+  port = 443
+  pool = "/Common/intraPool"
+  profiles = ["/Common/tcp","/Common/inranetSSL","/Common/http"]
+  source_address_translation = "automap"
+  translate_address = "enabled"
+  translate_port = "enabled"
+  vlans_disabled = true
+}
+
+resource "bigip_ltm_virtual_server" "http" {
+  name = "/Common/wwwStatic_https"
+  destination = "10.33.8.23"
+  port = 80
+  pool = "/Common/intraPool"
+  profiles = ["/Common/tcp","/Common/http"]
+  source_address_translation = "automap"
+  translate_address = "enabled"
+  translate_port = "enabled"
+  vlans_disabled = true
+}
